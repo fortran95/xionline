83f079b65554843bcbdcc2550a090bb5898754d1
lucifer <fortran95@126.com>
2013-01-09 12:14:14 +0800

    prototype certificate

--- class/certificate.php
+++ class/certificate.php
@@ -3,5 +3,21 @@ class Certificate{
     public function __construct($certtext=''){
         $this->certtext = $certtext;
     }
+    public function sign($text,$passphrase){
+    }
+    public function verify($signature,$text){
+    }
+    public function encrypt($text){
+    }
+    public function decrypt($ciphertext,$passphrase){
+    }
+    private function _parseCert(){
+        /*
+         * Parse $this->certtext and refresh all info
+         * stored in this class.
+         *
+         */
+#        require_once(
+    }
 }
 ?>
