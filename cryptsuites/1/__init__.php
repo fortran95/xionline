9ef7671688391d5c33e22593a95b5dd53ebe0eab
lucifer <fortran95@126.com>
2013-01-11 01:27:23 +0800

    doing XML certificate parsing

--- cryptsuites/1/__init__.php
+++ cryptsuites/1/__init__.php
@@ -9,4 +9,6 @@ include("$_cryptsuite_1_basepath/pkciphers/__init__.php");
 
 include("$_cryptsuite_1_basepath/certificate.php");
 include("$_cryptsuite_1_basepath/keyblock.php");
+
+$_cryptsuite_1_standard_path = "$_cryptsuite_1_basepath/standards";
 ?>
--- cryptsuites/1/certificate.php
+++ cryptsuites/1/certificate.php
@@ -1,9 +1,37 @@
 <?
-class Certificate{
 
+class Certificate{
     public function __construct($certtext=''){
+        global $_cryptsuite_1_standard_path;
+        $this->standardsPath = $_cryptsuite_1_standard_path;
+
         $this->certtext = $certtext;
 
+        try{
+            $this->dom = new DOMDocument();
+            $this->loadXML($this->certtext);
+            if(!$xml->schemaValidate($this->standardsPath . '/certificate.xsd'))
+                throw new Exception();
+        }catch(Exception $e){
+            throw new CryptoException("given certificate invalid.");
+        }
+    }
+    public function __get($name){
+        if(!isset($this->$name)){  # parse certificate on demand
+            switch($name){  # trigger parsing function
+                case "id":
+                    break;
+                default:
+                    return false;
+            }
+        }
+        return $this->$name;
+    }
+    public function __set($name,$value){
+        # each attribute can be set only once.
+        if(isset($this->$name))
+            return;
+        $this->$name = $value;
     }
     private function parseCert(){
         /*
