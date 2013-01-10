6c3501e749397e35c70c281142de638be10e2145
lucifer <fortran95@126.com>
2013-01-10 22:33:08 +0800

    enhanced encapsuling

--- cryptsuites/1/class/ciphertext.php
+++ cryptsuites/1/class/ciphertext.php
@@ -9,28 +9,30 @@ class cipherText{
      * result, or new.
      *
      */
-    function __construct($text){
-        try{
-            $json = json_decode($text);
-            if($json && array_key_exists('t',$json) && array_key_exists('d',$json)){
-                print_r($json);
-                $this->_text = $json->t;
-                $this->_data = array();
-                foreach($json->d as $key=>$value){
-                    $this->_data[$key] = $value;
+    function __construct($text,$encode=False){
+        if(!$encode){
+            try{
+                $json = json_decode($text);
+                if($json && array_key_exists('t',$json) && array_key_exists('d',$json)){
+                    $this->_text = $json->t;
+                    $this->_data = array();
+                    foreach($json->d as $key=>$value)
+                        $this->_data[$key] = $value;
                 }
-                return;
+            }catch(Exception $e){
+                throw CryptoException("failed parsing ciphertext block.");
             }
-        }catch(Exception $e){}
-        $this->_text = $text;
-        $this->_data = array();
+        } else {
+            $this->_text = $text;
+            $this->_data = array();
+        }
+        $this->_encode = $encode;
     }
     function __set($name,$value){
         if(substr($name,0,1) != '_'){
             $this->_data[$name] = $value;
             return;
         }
-
         $this->$name = $value;
         return;
     }
@@ -44,19 +46,23 @@ class cipherText{
         return $this->$name;
     }
     function __toString(){
-        return json_encode(array('t'=>$this->_text,
-                                 'd'=>$this->_data,));
+        if($this->_encode)
+            return json_encode(array('t'=>base64_encode($this->_text),
+                                     'd'=>$this->_data,));
+        else
+            return base64_decode($this->_text);
     }
 }
 /*
-$test = new cipherText('this is some text.');
+$test = new cipherText(md5('',True),True);
 $test->digestmod = 'sha1';
 $test->length = 10;
 
 $str = sprintf("%s",$test);
 print_r($str);
 
-$t2 = new cipherText($str);
+$t2 = new cipherText($str,False);
 print $t2->length;
+print sprintf("%s",$t2);
 */
 ?>
